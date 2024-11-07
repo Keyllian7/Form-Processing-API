efe293456ba4590f51f46f1fb015ceb95afa970b
Keyllian7 <keyllianazevedo2@gmail.com>
2024-11-07 19:30:48 -0300

    Configuração inicial do spring boot

---
+++ form-processing/src/test/java/projeto/com/br/form_processing/FormProcessingApplicationTests.java
@@ -0,0 +1,13 @@
+package projeto.com.br.form_processing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormProcessingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
