47ec5b2406584d898b269612cb416708d6158d44
Keyllian Azevedo <157046442+Keyllian7@users.noreply.github.com>
2024-11-16 16:24:03 -0300

    Ajuste na criação do token

--- form-processing/src/main/java/projeto/com/br/form/processing/infra/security/TokenService.java
+++ form-processing/src/main/java/projeto/com/br/form/processing/infra/security/TokenService.java
@@ -22,7 +22,7 @@ public class TokenService {
             Algorithm algorithm = Algorithm.HMAC256(secret);
             String token = JWT.create()
                     .withIssuer("form-processing")
-                    .withSubject(user.getId())
+                    .withSubject(user.getEmail())
                     .withExpiresAt(genExpirationDate())
                     .sign(algorithm);
             return token;
