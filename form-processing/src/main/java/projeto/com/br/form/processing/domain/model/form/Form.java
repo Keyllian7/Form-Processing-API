579fdb472eda5d514c571d0be8d9facc407d414f
Keyllian Azevedo <157046442+Keyllian7@users.noreply.github.com>
2024-11-18 11:44:16 -0300

    Relacionamento entre formulário e usuário

--- form-processing/src/main/java/projeto/com/br/form/processing/domain/model/form/Form.java
+++ form-processing/src/main/java/projeto/com/br/form/processing/domain/model/form/Form.java
@@ -5,11 +5,12 @@ import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import projeto.com.br.form.processing.domain.model.user.User;
 
 import java.util.Date;
 
 @Table
-@Entity(name = "form")
+@Entity
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
@@ -25,11 +26,16 @@ public class Form {
     private String mensagem;
     private Date dataCriacao;
 
-    public Form(FormRequestDTO data) {
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User emissor;
+
+    public Form(FormRequestDTO data, User emissor) {
         this.motivo = data.motivo();
         this.setor = data.setor();
         this.mensagem = data.mensagem();
         this.dataCriacao = new Date();
+        this.emissor = emissor;
     }
 
 }
