3f7d29463d555b65409f46ceda0b585085a8c0ef
Romeu Lucas <romeulucas50@hotmail.com>
2024-11-19 20:42:59 -0300

    alterado id do ticket de uuid para id

--- form-processing/src/main/java/projeto/com/br/form/processing/api/dto/form/FormResponseDTO.java
+++ form-processing/src/main/java/projeto/com/br/form/processing/api/dto/form/FormResponseDTO.java
@@ -5,6 +5,6 @@ import projeto.com.br.form.processing.domain.model.user.User;
 
 import java.util.Date;
 
-public record FormResponseDTO (String id, String motivo, String mensagem, Date dataCriacao, User emissor) {
+public record FormResponseDTO (Long id, String motivo, String mensagem, Date dataCriacao, User emissor) {
     public FormResponseDTO(Form form) {this(form.getId(), form.getMotivo(), form.getMensagem(), form.getDataCriacao(), form.getEmissor());}
 }
